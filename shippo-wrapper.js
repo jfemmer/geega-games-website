5ebe040536392352ceadcb46b5c5e2258efe28bd
jfemmer <65245024+jfemmer@users.noreply.github.com>
2025-05-17 00:36:14 -0500

    Update shippo-wrapper.js

--- shippo-wrapper.js
+++ shippo-wrapper.js
@@ -1,5 +1,10 @@
 module.exports = async () => {
   const shippoModule = await import('shippo');
-  const shippo = shippoModule.default; // default export is the function
-  return shippo(process.env.SHIPPO_TEST_KEY); // ✅ CALL, don't use `new`
+  const Shippo = shippoModule.Shippo || shippoModule.default?.Shippo;
+
+  if (!Shippo) {
+    throw new Error('Shippo constructor not found in module export');
+  }
+
+  return new Shippo(process.env.SHIPPO_TEST_KEY);
 };

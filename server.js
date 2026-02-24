11102faacc4096ff6695283120b591ebe846016f
jfemmer <65245024+jfemmer@users.noreply.github.com>
2026-02-23 23:55:13 -0600

    Update server.js

--- server.js
+++ server.js
@@ -163,9 +163,9 @@ async function cropAndPrepNameBar(originalPath, outPath, useThreshold = false) {
       ? CROP.nameBar
       : {
   left: Math.floor(W * 0.08),   // was 0.05
-  top: Math.floor(H * 0.01),   // was 0.03
+  top: Math.floor(H * 0.05),   // was 0.03
   width: Math.floor(W * 0.75),  // was 0.90
-  height: Math.floor(H * 0.09), // was 0.16
+  height: Math.floor(H * 0.08), // was 0.16
 }
 
   let pipeline = sharp(originalPath)

4a63b3e8a903f9eb1026ca437db0cfeb873632b8
jfemmer <65245024+jfemmer@users.noreply.github.com>
2026-02-23 23:50:06 -0600

    Update server.js

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ async function cropAndPrepNameBar(originalPath, outPath, useThreshold = false) {
       ? CROP.nameBar
       : {
   left: Math.floor(W * 0.08),   // was 0.05
-  top: Math.floor(H * 0.025),   // was 0.03
+  top: Math.floor(H * 0.01),   // was 0.03
   width: Math.floor(W * 0.75),  // was 0.90
   height: Math.floor(H * 0.09), // was 0.16
 }

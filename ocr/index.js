501e4f35e2d058e19e3715987a09efbcae291763
jfemmer <65245024+jfemmer@users.noreply.github.com>
2026-03-19 14:13:04 -0500

    Update index.js

--- ocr/index.js
+++ ocr/index.js
@@ -1,9 +1,15 @@
 const { ocrCardNameHighAccuracy } = require("./nameOcr");
 const { ocrCollectorNumberHighAccuracy } = require("./collectorOcr");
-const { pickPrintingByCollectorLocal, refineByArtworkHashLocal } = require("./localPicker");
 const { computeOverallScore, shouldAutoIngest } = require("./confidenceGate");
 const { enqueueForReview } = require("./reviewQueue");
 const { detectSetSymbol, ensureSetSymbolCache } = require("./setSymbolMatcher");
+
+let pickPrintingByCollectorLocal = null;
+let refineByArtworkHashLocal = null;
+
+if (process.env.USE_LOCAL === "true") {
+  ({ pickPrintingByCollectorLocal, refineByArtworkHashLocal } = require("./localPicker"));
+}
 
 module.exports = {
   ocrCardNameHighAccuracy,
